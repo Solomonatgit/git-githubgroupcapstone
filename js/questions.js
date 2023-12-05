a03fc0178822349c17497f7544799b834dca9f91
solomonatgit <treped69@outlook.com>
2023-12-05 18:50:35 +0200

    first

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyperlinks and Text Markup Language",
+      "Hyper Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyper Tool Markup Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which tag is used to create a line break in HTML?",
+    answer: "br",
+    options: [
+      "lb",
+      "br",
+      "break",
+      "line"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which attribute is used to define the URL of a hyperlink in HTML?",
+    answer: "href",
+    options: [
+      "src",
+      " link",
+      "href",
+      " url"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which CSS property is used to change the text color of an element?",
+    answer: "color",
+    options: [
+      "text-color",
+      "font-color",
+      "text-style",
+      "color"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which CSS property is used to set the background color of an element?",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "background-color",
+      "color-background",
+      "bg-color"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which CSS property is used to remove the underlines from links?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "text-decoration: none;",
+      "underline: none;",
+      "text-style: none;",
+      "link-decoration: none;"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    answer: "All The Above",
+    options: [
+      "Var",
+      "Let",
+      "Const",
+      "All The Above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which symbol is used for single-line comments in JavaScript?",
+    answer: "//",
+    options: [
+      "**",
+      "##",
+      "--",
+      "//"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What will be the output of console.log(typeof([]))?",
+    answer: "object",
+    options: [
+      "object",
+      "array",
+      "list",
+      "undefined"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What keyword is used to declare a variable that cannot be reassigned a new value?",
+    answer: "const",
+    options: [
+      "let",
+      "var",
+      "const",
+      "set"
+    ]
+  },
+
+];
